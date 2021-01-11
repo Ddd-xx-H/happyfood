3fa63c663a70aeaf7a8784ef7bd968e09e4ab9a3
Ddd-xx-H <1312679402@qq.com>
2021-01-11 18:12:30 +0800

    ccccc

---
+++ components/Lunbo.vue
@@ -0,0 +1,45 @@
+<template>
+  <el-carousel :interval="4000" arrow="always" height="540px">
+    <el-carousel-item v-for="item in 6" :key="item">
+     
+    </el-carousel-item>
+  </el-carousel>
+</template>
+
+<style>
+ .el-carousel__item{
+  background-size: cover;
+  
+ }
+   
+  
+  .el-carousel__item:nth-child(2n) {
+ 
+   background-image: url(../assets/food-1.jpg);
+ 
+  }
+
+   .el-carousel__item:nth-child(2n+1) {
+      
+   background-image: url(../assets/food-2.jpg);
+   
+  }
+    .el-carousel__item:nth-child(6) {
+       
+   background-image: url(../assets/food-3.jpg);
+  
+  }
+     .el-carousel__item:nth-child(5) {
+       
+   background-image: url(../assets/food-4.jpg);
+  }
+     .el-carousel__item:nth-child(4) {
+     
+   background-image: url(../assets/food-5.jpg);
+  }
+      .el-carousel__item:nth-child(3) {
+     
+   background-image: url(../assets/food-6.jpg);
+  }
+  
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,20 +1,85 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">
-        happyfood
-      </h1>
-      
- 
+<div>
+  <div class="title">
+    <div class="nav">
+      <div class="vv">
+        <el-link href="https://element.eleme.io" :underline="false"
+          >首页</el-link
+        >
+        <el-link href="https://element.eleme.io" :underline="false"
+          >花样菜谱</el-link
+        >
+        <el-link href="https://element.eleme.io" :underline="false"
+          >营养百科</el-link
+        >
+           <el-link href="https://element.eleme.io" :underline="false"
+          >
+          <img style="width: 40px; height: 40px" src="../images/logo.png" />
+          </el-link
+        >
+        <el-link href="https://element.eleme.io" :underline="false"
+          >家庭趣玩</el-link
+        >
+        <el-link href="https://element.eleme.io" :underline="false"
+          >抖音同款</el-link
+        >
+        <el-link href="https://element.eleme.io" :underline="false"
+          >小知识</el-link
+        >
+      </div>
     </div>
   </div>
+  <div class="lunbo">
+  <Lunbo />
+  </div>
+  <div class="middle">
+     <div class="middletext"></div>
+  </div>
+</div>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
 <style lang="less" scoped>
+.title {
+  width: 100%;
+  height: 68px;
+  background-color: #F4505B;
+}
+.nav {
+  width: 100%;
+  // margin: 0 auto;
+  height: 68px;
+  // background-color: gold;
+  line-height: 68px;
+}
+.el-link {
+  padding-left: 20px;
+  padding-right: 20px;
+  font-size: 18px;
 
+  color: #ffffff;
+}
+.vv {
+  max-width: 740px;
+  // background-color: green;
+  margin: 0 auto;
+}
+.lunbo{
+ width: 100%;
+}
+.middle{
+  width: 1200px;
+  height: 200px;
+  background-color: turquoise;
+  margin: 0 auto;
+}
+.middletext{
+   max-width: 400px;
+   height: 200px;
+   margin: 0 auto;
+   background-color: yellowgreen;
+}
 </style>
